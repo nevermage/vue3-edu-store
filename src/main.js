1b4ac3fbe9fb27fd0fa284ace667432aa5339c80
nevermage <maksim14032002@gmail.com>
2021-11-12 17:38:08 +0200

    vue cart

--- src/main.js
+++ src/main.js
@@ -10,32 +10,32 @@ const app = createApp({
     data: () => ({
        cartProducts: [],
     }),
-    methods: {
-        addToCart(product) {
-            if (this.cartProducts.find((i, index)=> i==product)) {
-                alert("already in cart");
-            } else {
-                product['quantity'] = 1;
-                this.cartProducts.push(product);
-            }
-        },
-        deleteFromCart(product) {
-            this.cartProducts.splice(this.cartProducts.indexOf(product), 1)
-        },
-        cartPlusQuantity(product) {
-            let x = this.cartProducts.indexOf(product);
-            this.cartProducts[x]['quantity']++;
-        },
-        cartMinusQuantity(product) {
-            let x = this.cartProducts.indexOf(product);
-            this.cartProducts[x]['quantity']--;
-        },
-        formSubmit() {
-            console.log(this.cartProducts);
-            this.cartProducts = [];
-
-        },
-    }
+    // methods: {
+    //     addToCart(product) {
+    //         if (this.cartProducts.find((i, index)=> i==product)) {
+    //             alert("already in cart");
+    //         } else {
+    //             product['quantity'] = 1;
+    //             this.cartProducts.push(product);
+    //         }
+    //     },
+    //     deleteFromCart(product) {
+    //         this.cartProducts.splice(this.cartProducts.indexOf(product), 1)
+    //     },
+    //     cartPlusQuantity(product) {
+    //         let x = this.cartProducts.indexOf(product);
+    //         this.cartProducts[x]['quantity']++;
+    //     },
+    //     cartMinusQuantity(product) {
+    //         let x = this.cartProducts.indexOf(product);
+    //         this.cartProducts[x]['quantity']--;
+    //     },
+    //     formSubmit() {
+    //         console.log(this.cartProducts);
+    //         this.cartProducts = [];
+    //
+    //     },
+    // }
 });
 
 app.use(store);
